ad507b7f1455223abd9616f470322b801676babf
xasterKies <samueledward40@gmail.com>
2023-06-23 13:31:02 +0100

    feat: setting up CLI actions

--- cmd/root.go
+++ cmd/root.go
@@ -1,30 +1,50 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"io"
 	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/xasterKies/pomanalyzer/app"
+	"github.com/xasterKies/pomanalyzer/pomodoro"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "pomanalyzer",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Interactive Pomodoro Timer",
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	 RunE: func(cmd *cobra.Command, args []string) error { 
+		repo, err := getRepo()
+		if err != nil {
+			return err
+		}
+
+		config := pomodoro.NewConfig(
+			repo,
+			viper.GetDuration("pomo"),
+			viper.GetDuration("short"),
+			viper.GetDuration("long"),
+		)
+
+		return rootAction(os.Stdout, config)
+	 },
+}
+
+func rootAction(out io.Writer, config *pomodoro.IntervalConfig) error {
+	a, err := app.New(config)
+	if err != nil {
+		return err
+	} 
+	
+	return a.Run()
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -37,11 +57,17 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
+	
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.pomanalyzer.yaml)")
+
+	rootCmd.Flags().DurationP("pomo", "p", 25*time.Minute, "Pomodoro duration")
+	rootCmd.Flags().DurationP("short", "s", 5*time.Minute, "Short break duration")
+	rootCmd.Flags().DurationP("long", "l", 15*time.Minute, "Long break duration")
+
+	viper.BindPFlag("pomo", rootCmd.Flags().Lookup("pomo"))
+	viper.BindPFlag("short", rootCmd.Flags().Lookup("short"))
+	viper.BindPFlag("long", rootCmd.Flags().Lookup("long"))
+
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
