d347acdb0cdbb8ee2ef81be6c910da801eb27805
xasterKies <samueledward40@gmail.com>
2023-06-23 13:01:09 +0100

    chore: getRepo() func

---
+++ cmd/repoinmemory.go
@@ -0,0 +1,10 @@
+package cmd
+
+import (
+	"github.com/xasterKies/pomanalyzer/pomodoro"
+	"github.com/xasterKies/pomanalyzer/repository"
+)
+
+func getRepo() (pomodoro.Repository, error) {
+	return repository.NewInMemoryRepo(), nil
+}
