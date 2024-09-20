8652fc25ae0a0ff760226578b540fd90c88e58f5
xasterKies <samueledward40@gmail.com>
2024-09-20 10:04:47 +0100

    feat: all repository functions added

--- repository/inMemory.go
+++ repository/inMemory.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"fmt"
 	"sync"
 
 	"github.com/xasterKies/pomanalyzer/pomodoro"
@@ -25,4 +26,48 @@ func (r *inMemoryRepo) Create(i pomodoro.Interval) (int64, error) {
 	r.intervals = append(r.intervals, i)
 
 	return i.ID, nil
+}
+
+func (r *inMemoryRepo) Update(i pomodoro.Interval) error {
+	r.Lock()
+	defer r.Unlock()
+	if i.ID == 0 {
+		return fmt.Errorf("%w: %d", pomodoro.ErrInvalidID, i.ID)
+	}
+
+	r.intervals[i.ID-1] = i
+	return nil
+}
+
+func (r *inMemoryRepo) ByID(id int64) (pomodoro.Interval, error) {
+	r.RLock()
+	defer r.RUnlock()
+	i := pomodoro.Interval{}
+	if id == 0 {
+		return i, fmt.Errorf("%w: %d", pomodoro.ErrInvalidID, id)
+	}
+
+	i = r.intervals[id-1]
+
+	return i, nil
+}
+
+func (r *inMemoryRepo) Breaks(n int) ([]pomodoro.Interval, error) {
+	r.RLock()
+	defer r.RUnlock()
+	data := []pomodoro.Interval{}
+
+	for k := len(r.intervals) - 1; k >= 0; k-- {
+		if r.intervals[k].Category == pomodoro.CategoryPomodoro {
+			continue
+		}
+
+		data = append(data, r.intervals[k])
+
+		if len(data) == n {
+			return data, nil
+		}
+	}
+
+	return data, nil
 }
