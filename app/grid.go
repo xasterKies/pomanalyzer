132d5f90abfc283c882c00ed9f77a7db066efeea
xasterKies <samueledward40@gmail.com>
2024-09-23 10:12:46 +0100

    feat: finishing up terminal ui and setting up cobra CLI

---
+++ app/grid.go
@@ -0,0 +1,77 @@
+package app
+
+import (
+  "github.com/mum4k/termdash/align"
+  "github.com/mum4k/termdash/container"
+  "github.com/mum4k/termdash/container/grid"
+  "github.com/mum4k/termdash/linestyle"
+  "github.com/mum4k/termdash/terminal/terminalapi"
+)
+
+func newGrid(b *buttonSet, w *widgets,
+  t terminalapi.Terminal) (*container.Container, error) {
+
+  builder := grid.New()
+
+  // Add first row
+  builder.Add(
+    grid.RowHeightPerc(30,
+      grid.ColWidthPercWithOpts(30,
+        []container.Option{
+          container.Border(linestyle.Light),
+          container.BorderTitle("Press Q to Quit"),
+        },
+        // Add inside row
+        grid.RowHeightPerc(80,
+          grid.Widget(w.donTimer)),
+        grid.RowHeightPercWithOpts(20,
+          []container.Option{
+            container.AlignHorizontal(align.HorizontalCenter),
+          },
+          grid.Widget(w.txtTimer,
+            container.AlignHorizontal(align.HorizontalCenter),
+            container.AlignVertical(align.VerticalMiddle),
+            container.PaddingLeftPercent(49),
+          ),
+        ),
+      ),
+      grid.ColWidthPerc(70,
+        grid.RowHeightPerc(80,
+          grid.Widget(w.disType, container.Border(linestyle.Light)),
+        ),
+        grid.RowHeightPerc(20,
+          grid.Widget(w.txtInfo, container.Border(linestyle.Light)),
+        ),
+      ),
+    ),
+  )
+
+  // Add second row
+  builder.Add(
+    grid.RowHeightPerc(10,
+      grid.ColWidthPerc(50,
+        grid.Widget(b.btStart),
+      ),
+      grid.ColWidthPerc(50,
+        grid.Widget(b.btPause),
+      ),
+    ),
+  )
+
+  // Add third row
+  builder.Add(
+    grid.RowHeightPerc(60),
+  )
+
+  gridOpts, err := builder.Build()
+  if err != nil {
+    return nil, err
+  }
+
+  c, err := container.New(t, gridOpts...)
+  if err != nil {
+    return nil, err
+  }
+
+  return c, nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,20 @@
 module github.com/xasterKies/pomanalyzer
 
 go 1.21.4
+
+require github.com/mum4k/termdash v0.13.0
+
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
+
+require (
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.0.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.0.3 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/spf13/cobra v1.8.1
+	golang.org/x/sys v0.0.0-20201113233024-12cec1faf1ba // indirect
+	golang.org/x/text v0.3.4 // indirect
+)
