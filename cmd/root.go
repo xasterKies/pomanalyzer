4a681cfe97ec9fbe06ae43edf83da7b17b896f09
xasterKies <samueledward40@gmail.com>
2024-09-23 10:55:54 +0100

    feat: CLI config with viper

--- cmd/root.go
+++ cmd/root.go
@@ -1,51 +1,105 @@
 /*
 Copyright © 2024 xasterKies
-
 */
 package cmd
 
 import (
+	"fmt"
+	"io"
 	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	"github.com/xasterKies/pomanalyzer/app"
+	"github.com/xasterKies/pomanalyzer/pomodoro"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
 )
 
-
+var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "pomanalyzer",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+  Use:   "pomo",
+  Short: "Interactive Pomodoro Timer",
+  // Uncomment the following line if your bare application
+  // has an action associated with it:
+  //  Run: func(cmd *cobra.Command, args []string) { },
+  RunE: func(cmd *cobra.Command, args []string) error {
+    repo, err := getRepo()
+    if err != nil {
+      return err
+    }
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+    config := pomodoro.NewConfig(
+      repo,
+      viper.GetDuration("pomo"),
+      viper.GetDuration("short"),
+      viper.GetDuration("long"),
+    )
+    return rootAction(os.Stdout, config)
+  },
+}
+
+func rootAction(out io.Writer, config *pomodoro.IntervalConfig) error {
+  a, err := app.New(config)
+  if err != nil {
+    return err
+  }
+
+  return a.Run()
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
-	}
+  if err := rootCmd.Execute(); err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
+  cobra.OnInitialize(initConfig)
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.pomanalyzer.yaml)")
+  rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "",
+    "config file (default is $HOME/.pomo.yaml)")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+  rootCmd.Flags().DurationP("pomo", "p", 25*time.Minute, 
+                            "Pomodoro duration")
+  rootCmd.Flags().DurationP("short", "s", 5*time.Minute, 
+                            "Short break duration")
+  rootCmd.Flags().DurationP("long", "l", 15*time.Minute, 
+                            "Long break duration")
+
+  viper.BindPFlag("pomo", rootCmd.Flags().Lookup("pomo"))
+  viper.BindPFlag("short", rootCmd.Flags().Lookup("short"))
+  viper.BindPFlag("long", rootCmd.Flags().Lookup("long"))
 }
 
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+  if cfgFile != "" {
+    // Use config file from the flag.
+    viper.SetConfigFile(cfgFile)
+  } else {
+    // Find home directory.
+    home, err := homedir.Dir()
+    if err != nil {
+      fmt.Println(err)
+      os.Exit(1)
+    }
 
+    // Search config in home directory with name ".pomo" (without extension).
+    viper.AddConfigPath(home)
+    viper.SetConfigName(".pomo")
+  }
+
+  viper.AutomaticEnv() // read in environment variables that match
+
+  // If a config file is found, read it in.
+  if err := viper.ReadInConfig(); err == nil {
+    fmt.Println("Using config file:", viper.ConfigFileUsed())
+  }
+}
--- go.mod
+++ go.mod
@@ -5,8 +5,26 @@ go 1.21.4
 require github.com/mum4k/termdash v0.13.0
 
 require (
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/sagikazarmark/locafero v0.4.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
+	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.19.0 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
@@ -15,6 +33,6 @@ require (
 	github.com/lucasb-eyer/go-colorful v1.0.3 // indirect
 	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/spf13/cobra v1.8.1
-	golang.org/x/sys v0.0.0-20201113233024-12cec1faf1ba // indirect
-	golang.org/x/text v0.3.4 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
